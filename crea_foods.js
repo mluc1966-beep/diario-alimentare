f52dabe922f819c40e6fe62446723c8fb875aa15
mluc1966-beep <mluc1966@gmail.com>
2026-08-10 22:40:29 +0200

    Add files via upload

---
+++ crea_foods.js
@@ -0,0 +1,3 @@
+// Generato automaticamente in fase di pubblicazione da tools/build_crea.py
+// Fonte: CREA Centro di ricerca Alimenti e Nutrizione - https://www.alimentinutrizione.it
+window.CREA_FOODS = window.CREA_FOODS || [];
